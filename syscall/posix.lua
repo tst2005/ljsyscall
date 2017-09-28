398607a77ca4d6e7cda2e30d280bd9e0b87facfa
TsT <tst2005@gmail.com>
2017-09-28 14:31:36 +0200

    add url to lua-posix doc

--- syscall/posix.lua
+++ syscall/posix.lua
@@ -1,8 +1,10 @@
-local M={}
 
 local TODO=function()end
 
+local io = require "io"
+
 local S = require "syscall"
+
 -- see test.helpers
 local function Sassert(cond, err, ...)
   if not cond then
@@ -14,34 +16,49 @@ local function Sassert(cond, err, ...)
   return cond, ...
 end
 
+local M={}
 
-local io = require "io"
 
+-- little hack to allow posix.fileno[io.stdin] ...
 local filenomap = {
 	[io.stdin] = 0,
 	[io.stdout] = 1,
 	[io.stderr] = 2,
 }
+-- http://luaposix.github.io/luaposix/modules/posix.stdio.html#fileno
 M.fileno = function(fd) return filenomap[fd] end
 
+-- http://luaposix.github.io/luaposix/modules/posix.unistd.html#dup2
 M.dup2 = function(...) return S.dup2(...) end
+
+-- http://luaposix.github.io/luaposix/modules/posix.unistd.html#close
 M.close = function(fd) assert(fd.close) return fd:close() end
+
+-- http://luaposix.github.io/luaposix/modules/posix.unistd.html#read
 M.read = function(fd, size)
 	assert(fd.read)
 	return fd:read(nil, size)
 end
+
+-- http://luaposix.github.io/luaposix/modules/posix.unistd.html#write
 M.write = function(fd, ...) assert(fd.write) return fd:write(...) end
 
+-- http://luaposix.github.io/luaposix/modules/posix.unistd.html#pipe
 M.pipe = function()
 	local pr, pw = Sassert( S.pipe() )
 	assert(pr and pw)
 	return pr, pw
 end
 
+-- http://luaposix.github.io/luaposix/modules/posix.unistd.html#fork
 M.fork = function()
 	local pid = Sassert(S.fork())
 	return pid
 end
+-- http://luaposix.github.io/luaposix/modules/posix.unistd.html#_exit
+M._exit = TODO
+
+-- http://luaposix.github.io/luaposix/modules/posix.sys.wait.html#wait
 M.wait = function()
 	--local rpid, status = Sassert(S.wait())
 	local rpid, status = Sassert(S.waitpid(-1, 0))
@@ -65,12 +82,12 @@ end
 --    int exit status, or signal number responsible for "killed" or "stopped"
 
 
-
+-- http://luaposix.github.io/luaposix/modules/posix.unistd.html#exec
+-- http://luaposix.github.io/luaposix/modules/posix.unistd.html#execp
 M.execp = function(path, ...)
 	return Sassert( S.execvpe(path, path, {...}, {}) )
 end
 
-M._exit = TODO
 
 M._VERSION = "syscall.posix v0.1.0-alpha"
 M.version = M._VERSION
